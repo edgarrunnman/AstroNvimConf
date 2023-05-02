67242731015ba9d208facae9858368304088c2ad
Edgar Runnman <edgar.runnman@lm.se>
2023-05-02 21:57:11 +0200

    keybindings rust pack ts pack

--- mappings.lua
+++ mappings.lua
@@ -4,11 +4,7 @@
 -- lower level configuration and more robust one. (which-key will
 -- automatically pick-up stored data by this setting.)
 return {
-  -- first key is the mode
   n = {
-    -- second key is the lefthand side of the map
-    -- mappings seen under group name "Buffer"
-    ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
         require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
@@ -18,11 +14,50 @@ return {
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
+
     -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+
+    -- reload
+    ["<leader>r"] = { ":so %<CR>", desc = "Reload"},
+    ["<leader>R"] = { ":AstroReload<CR>", desc = "Reloads config" },
+  
+    -- tabs
+    ["<A-S-k>"] = { "<cmd>tabnext<CR>", desc = "Next tab" },
+    ["<A-S-j>"] = { "<cmd>tabprevious<CR>", desc = "Prev tab" }, 
+    ["<A-S-l>"] = { "<cmd>tabnew<cr>", desc = "New tab" },
+    ["<A-S-d>"] = { "<cmd>tabclose<CR>", desc = "Close tab" },
+
+    -- windows 
+    ["<A-l>"] = { "<C-w>l", desc = "next window" }, 
+    ["<A-h>"] = { "<C-w>h", desc = "prev window" },
+    --["<A-q>"] = { ":q<cr>", desc = "close window" },
+    ["<A-p>"] = { "<C-w>v", desc = "vertical split" },
+    ["<A-o>"] = { "<C-w>", desc = "horizontal split" },
+    ["<A-m>"] = { "<cmd>vertical resize -10<cr>", desc = "Decrease window width" },
+    ["<A-,>"] = { "<cmd>vertical resize +5<cr>", desc = "Increase window width" },
+    
+    -- buffers
+    ["<A-j>"] = { "<cmd>bprevious<cr>", desc = "Prev buffer" },
+    ["<A-k>"] = { "<cmd>bnext<cr>", desc = "Next buffer" },
+    ["<A-d>"] = { "<cmd>bd<cr>", desc = "Close buffer" },
+
+    -- terminal 
+    ["<A-z>"] = { "<cmd>terminal<cr>", desc = "new Terminal"},
+
+    -- echo test
+    ["<leader>a"] = { "<cmd>echo 'Hello'<cr>", desc = "test keymap"},
+
   },
+
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
   },
+ 
+  i = {
+    ["kj"] = { "<Esc>", desc = "To normal mode"},
+    ["<A-c>"] = { "<leader>/", desc = "Comment line"}
+
+  }
 }
