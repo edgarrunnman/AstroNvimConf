9b5d9598c5f5e8075ea03b91dd8e9af4d0360371
Edgar <edgar.runnman@gmail.com>
2023-05-07 22:24:08 +0200

    cntr keybinds

--- mappings.lua
+++ mappings.lua
@@ -7,7 +7,9 @@ return {
   n = {
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(
+          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
+        )
       end,
       desc = "Pick to close",
     },
@@ -16,48 +18,54 @@ return {
     ["<leader>b"] = { name = "Buffers" },
 
     -- quick save
-    ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<C-s>"] = { ":w!<cr>", desc = "Save File" }, -- change description but the same command
 
     -- reload
-    ["<leader>r"] = { ":so %<CR>", desc = "Reload"},
+    ["<leader>r"] = { ":so %<CR>", desc = "Reload" },
     ["<leader>R"] = { ":AstroReload<CR>", desc = "Reloads config" },
-  
+
     -- tabs
     ["<A-S-k>"] = { "<cmd>tabnext<CR>", desc = "Next tab" },
-    ["<A-S-j>"] = { "<cmd>tabprevious<CR>", desc = "Prev tab" }, 
+    ["<A-S-j>"] = { "<cmd>tabprevious<CR>", desc = "Prev tab" },
     ["<A-S-l>"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<A-S-d>"] = { "<cmd>tabclose<CR>", desc = "Close tab" },
 
-    -- windows 
-    ["<A-l>"] = { "<C-w>l", desc = "next window" }, 
+    -- windows
+    ["<A-l>"] = { "<C-w>l", desc = "next window" },
     ["<A-h>"] = { "<C-w>h", desc = "prev window" },
     --["<A-q>"] = { ":q<cr>", desc = "close window" },
     ["<A-p>"] = { "<C-w>v", desc = "vertical split" },
     ["<A-o>"] = { "<C-w>", desc = "horizontal split" },
     ["<A-m>"] = { "<cmd>vertical resize -10<cr>", desc = "Decrease window width" },
     ["<A-,>"] = { "<cmd>vertical resize +5<cr>", desc = "Increase window width" },
-    
+
+    ["<C-l>"] = { "<C-w>l", desc = "next window" },
+    ["<C-h>"] = { "<C-w>h", desc = "prev window" },
+    --["<A-q>"] = { ":q<cr>", desc = "close window" },
+    ["<C-p>"] = { "<C-w>v", desc = "vertical split" },
+    ["<C-o>"] = { "<C-w>", desc = "horizontal split" },
+    ["<C-m>"] = { "<cmd>vertical resize -10<cr>", desc = "Decrease window width" },
+    ["<C-,>"] = { "<cmd>vertical resize +5<cr>", desc = "Increase window width" },
+
     -- buffers
     ["<A-j>"] = { "<cmd>bprevious<cr>", desc = "Prev buffer" },
     ["<A-k>"] = { "<cmd>bnext<cr>", desc = "Next buffer" },
     ["<A-d>"] = { "<cmd>bd<cr>", desc = "Close buffer" },
-
-    -- terminal 
-    ["<A-z>"] = { "<cmd>terminal<cr>", desc = "new Terminal"},
+    ["<C-j>"] = { "<cmd>bprevious<cr>", desc = "Prev buffer" },
+    ["<C-k>"] = { "<cmd>bnext<cr>", desc = "Next buffer" },
+    ["<C-d>"] = { "<cmd>bd<cr>", desc = "Close buffer" },
 
     -- echo test
-    ["<leader>a"] = { "<cmd>echo 'Hello'<cr>", desc = "test keymap"},
-
+    -- ["<leader>a"] = { "<cmd>echo 'Hello'<cr>", desc = "test keymap" },
   },
 
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
   },
- 
-  i = {
-    ["kj"] = { "<Esc>", desc = "To normal mode"},
-    ["<A-c>"] = { "<leader>/", desc = "Comment line"}
 
-  }
+  i = {
+    ["kj"] = { "<Esc>", desc = "To normal mode" },
+    ["<A-c>"] = { "<leader>/", desc = "Comment line" },
+  },
 }
